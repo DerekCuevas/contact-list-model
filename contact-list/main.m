6c1896b001a7d4dbd89e1b442eb22881304a818a
Derek Cuevas <dccuevas2@gmail.com>
2016-03-18 15:03:17 -0700

    first commit

---
+++ contact-list/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  contact-list
+//
+//  Created by Derek Cuevas on 3/18/16.
+//  Copyright © 2016 Derek Cuevas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
